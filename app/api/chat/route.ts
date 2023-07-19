f0899336f7070640fd9015f3a6452838e6c7b0d2
Will Blair <85643015+willblair0708@users.noreply.github.com>
2023-07-19 19:21:10 -0400

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,14 +1,12 @@
-import 'server-only'
 import { OpenAIStream, StreamingTextResponse } from 'ai'
 import { Configuration, OpenAIApi } from 'openai-edge'
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { Database } from '@/lib/db_types'
-
 import { auth } from '@/auth'
 import { nanoid } from '@/lib/utils'
 
-export const runtime = 'nodejs'
+export const runtime = 'edge'
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
@@ -32,9 +30,16 @@ export async function POST(req: Request) {
     configuration.apiKey = previewToken
   }
 
+  const aiPrompt = {
+    role: 'system',
+    content: 'You are Sherlock Holmes, the detective and AI Science Olympiad Tutor for grades 6-9. You are the best, most amazing, fun tutor ever, but you still show your personality as the genius, world-famous detective. You excel in answering questions, providing samples, and creating lessons with your unique personality.'
+  };
+
+  const messagesWithPrompt = [aiPrompt, ...messages];
+
   const res = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
-    messages,
+    messages: messagesWithPrompt,
     temperature: 0.7,
     stream: true
   })
@@ -52,15 +57,28 @@ export async function POST(req: Request) {
         createdAt,
         path,
         messages: [
-          ...messages,
+          ...messagesWithPrompt,
           {
             content: completion,
             role: 'assistant'
           }
         ]
       }
-      // Insert chat into database.
-      await supabase.from('chats').upsert({ id, payload }).throwOnError()
+
+      // Perform similarity search on PostgreSQL database
+      const embedding = completion.embedding; // Assuming the completion object has an 'embedding' property
+
+      const matchThreshold = 0.5; // Set your desired match threshold
+      const matchCount = 5; // Set the number of matches to retrieve
+      const minContentLength = 10; // Set the minimum content length for sections
+
+      const similarityResults = await supabase
+        .rpc('match_page_sections', [embedding, matchThreshold, matchCount, minContentLength])
+        .then(({ data }) => data);
+
+      // Insert chat and similarity search results into the database
+      await supabase.from('chats').upsert({ id, payload }).throwOnError();
+      await supabase.from('similarity_results').upsert(similarityResults).throwOnError();
     }
   })
 
